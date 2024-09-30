1f32c45b1fa15025353e6d1cd56ece47597d3b84
hiy0ung <hyejee98@gmail.com>
2024-09-30 12:28:36 +0900

    20240930

--- e_select/korea_db.sql
+++ e_select/korea_db.sql
@@ -28,6 +28,16 @@ CREATE TABLE `korea_db`.`members` (
     join_date DATE NOT NULL
 );
 
+create table `korea_db`.`purchases` (
+	purchase_id int auto_increment primary key,
+    member_id int not null,
+    product_code char(6) not null,
+    purchase_date date not null,
+    amount int not null,
+    quantity smallint not null,
+    foreign key (member_id) references `members` (member_id)
+);
+
 insert into `korea_db`.`members` (name, gender, area_code, grade, contact, points, join_date)
 values 
 	('Jin', 'Male', 'SEOUL', 'Bronze', '010-1234-5678', 100, '2023-01-01'),
@@ -40,6 +50,20 @@ values
 	('Eunji', 'Female', 'BUSAN', 'Gold', '010-8901-2345', null, '2024-01-08'),
 	('Kyung', 'Male', 'JEJU', 'Platinum', '010-9012-3456', 350, '2022-01-09'),
 	('Sooyoung', 'Female', 'JEJU', 'Diamond', '010-0123-4567', 600, '2018-01-10');
+    
+INSERT INTO `korea_db`.`purchases` (member_id, product_code, purchase_date, amount, quantity)
+VALUES
+	(1, 'P0001', '2024-01-15', 10000, 1),
+	(1, 'P0002', '2024-01-16', 35000, 2),
+	(3, 'P0003', '2024-01-17', 20000, 1),
+	(3, 'P0004', '2024-01-18', 15000, 3),
+	(3, 'P0005', '2024-01-19', 40000, 1),
+	(4, 'P0006', '2024-01-20', 35000, 5),
+	(7, 'P0007', '2024-01-21', 12000, 1),
+	(8, 'P0008', '2024-01-22', 13000, 2);
 
 desc `members`;
+desc `purchases`;
+
 select * from `members`;
+select * from `purchases`;
